8762881837ff52426b33fe0b5d22a22bd23922be
alham <alhamedAbdulrahman1@gmail.com>
2023-11-12 08:48:09 +0300

    upda site and base with adding a proper title

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   outDir: "./docs",
+  site: "https://alhamed1.github.io",
+  base: "/ommar-alardh-portfolio",
   integrations: [react(), tailwind()],
 });
